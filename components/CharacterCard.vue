2fae45dfdc3fc8ad5f4706664602a411923b286b
Andrea Castro <ap.castrocastellon@gmail.com>
2021-10-10 19:39:32 -0300

    first commit

---
+++ components/CharacterCard.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-card>
+    <div v-for="(character, index) of characters" :key="index">
+      <v-card-title class="headline">
+        {{ character.name }}
+      </v-card-title>
+      <img class="VuetifyLogo" alt="Vuetify Logo" :src="character.image" />
+    </div>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      characters: [
+        {
+          id: 1,
+          name: "Morty",
+          image: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
+        },
+        {
+          id: 2,
+          name: "Rick",
+          image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+        },
+        {
+          id: 3,
+          name: "Summer",
+          image: "https://rickandmortyapi.com/api/character/avatar/3.jpeg",
+        },
+        {
+          id: 4,
+          name: "Beth",
+          image: "https://rickandmortyapi.com/api/character/avatar/4.jpeg",
+        },
+        {
+          id: 5,
+          name: "Jerry",
+          image: "https://rickandmortyapi.com/api/character/avatar/5.jpeg",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <v-card class="logo py-4 d-flex justify-center">
+        <character-card :characters="characters" />
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
